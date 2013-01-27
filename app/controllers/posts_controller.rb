b813e2aeabe5ce8c4cb8345c4c684107588cbea3
Abraham Stimson <abraham.stimson@gmail.com>
2013-01-27 16:56:26 -0500

    Added markdown translation for post content.  When editing a post, you see html, but that's ok!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,6 +43,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post])
 
+    @markdown_content = Markdown.new(@post.content).to_html
+    @post.content = @markdown_content.html_safe
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
