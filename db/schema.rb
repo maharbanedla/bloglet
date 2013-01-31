c57895ef1e1ed20e05e162c224031e2c230ba8e1
Abraham Stimson <abraham.stimson@gmail.com>
2013-01-30 20:43:15 -0500

    Added ability to save drafts, with the appropriate confirmation messages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130127001922) do
+ActiveRecord::Schema.define(:version => 20130130041303) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130127001922) do
     t.boolean  "private"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "draft"
   end
 
 end
